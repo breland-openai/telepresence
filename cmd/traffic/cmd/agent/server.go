64ed3270af0926bebcdea7e3481727738e9ff3d7
Breland Miley 🌈 <breland@openai.com>
2026-05-07 23:16:04 -0700

    The One With Service Side Intercept Observability

--- cmd/traffic/cmd/agent/server.go
+++ cmd/traffic/cmd/agent/server.go
@@ -27,6 +27,11 @@ type awaitingForward struct {
 	doneCh   <-chan struct{}
 }
 
+const (
+	clientTunnelSlowAfter       = time.Second
+	clientTunnelStillWaitingLog = 5 * time.Second
+)
+
 func (s *state) Version(context.Context, *emptypb.Empty) (*rpc.VersionInfo2, error) {
 	return &rpc.VersionInfo2{Name: DisplayName, Version: version.Version}, nil
 }
@@ -65,10 +70,14 @@ func (s *state) Tunnel(server agent.Agent_TunnelServer) error {
 	if err != nil {
 		return errors.FromError(err, codes.FailedPrecondition, err.Error())
 	}
+	tunnelStart := time.Now()
 	if awc, ok := s.awaitingForwards.Load(stream.SessionID()); ok {
 		if awf, ok := awc.LoadAndDelete(stream.ID()); ok {
 			awf.streamCh <- stream
 			<-awf.doneCh
+			if elapsed := time.Since(tunnelStart); elapsed > clientTunnelSlowAfter {
+				clog.Warnf(ctx, "agent tunnel for awaited client stream stayed open for %s: session=%s conn=%s", elapsed.Round(time.Millisecond), stream.SessionID(), stream.ID())
+			}
 			return nil
 		}
 	}
@@ -82,6 +91,9 @@ func (s *state) Tunnel(server agent.Agent_TunnelServer) error {
 	endPoint := tunnel.NewDialer(stream, func() {}, ingressBytes, egressBytes)
 	endPoint.Start(ctx)
 	<-endPoint.Done()
+	if elapsed := time.Since(tunnelStart); elapsed > clientTunnelSlowAfter {
+		clog.Warnf(ctx, "agent tunnel endpoint stayed open for %s: session=%s conn=%s", elapsed.Round(time.Millisecond), stream.SessionID(), stream.ID())
+	}
 
 	if reporting {
 		s.ReportMetrics(ctx, &rpc.TunnelMetrics{
@@ -95,8 +107,11 @@ func (s *state) Tunnel(server agent.Agent_TunnelServer) error {
 
 func (s *state) WatchDial(session *rpc.SessionInfo, server agent.Agent_WatchDialServer) error {
 	ctx := server.Context()
-	clog.Debugf(ctx, "WatchDial called from client %s", session.SessionId)
-	defer clog.Debugf(ctx, "WatchDial ended from client %s", session.SessionId)
+	started := time.Now()
+	clog.Infof(ctx, "WatchDial called from client %s", session.SessionId)
+	defer func() {
+		clog.Infof(ctx, "WatchDial ended from client %s after %s: context=%v", session.SessionId, time.Since(started).Round(time.Millisecond), ctx.Err())
+	}()
 	drCh := make(chan *rpc.DialRequest)
 	sid := tunnel.SessionID(session.SessionId)
 	s.dialWatchers.Store(sid, drCh)
@@ -128,6 +143,7 @@ func (s *state) WatchDial(session *rpc.SessionInfo, server agent.Agent_WatchDial
 func (s *state) CreateClientStream(ctx context.Context, _ tunnel.Tag, sessionID tunnel.SessionID, id tunnel.ConnID, roundTripLatency, dialTimeout time.Duration,
 ) (tunnel.Stream, error) {
 	clog.Debugf(ctx, "Creating tunnel to client %s for id %s", sessionID, id)
+	createStart := time.Now()
 	var drCh chan<- *rpc.DialRequest
 	var awc *xsync.Map[tunnel.ConnID, *awaitingForward]
 	var aw *awaitingForward
@@ -155,6 +171,7 @@ func (s *state) CreateClientStream(ctx context.Context, _ tunnel.Tag, sessionID 
 		return fmt.Errorf("unable to create tunnel to client %s for id %s: no dial watcher", sessionID, id)
 	}, backoff.WithContext(backoff.NewConstantBackOff(20*time.Millisecond), ctx))
 	if err != nil {
+		clog.Warnf(ctx, "unable to create tunnel to client %s for id %s after %s: %v", sessionID, id, time.Since(createStart).Round(time.Millisecond), err)
 		return nil, err
 	}
 	defer func() {
@@ -179,13 +196,32 @@ func (s *state) CreateClientStream(ctx context.Context, _ tunnel.Tag, sessionID 
 		}
 	}
 
-	select {
-	case <-ctx.Done():
-		clog.Errorf(ctx, "unable to create tunnel to client %s for id %s after %s: %v", sessionID, id, time.Since(requestStart), ctx.Err())
-		return nil, ctx.Err()
-	case stream := <-stCh:
-		clog.Debugf(ctx, "Created tunnel to client %s for id %s in %s", sessionID, id, time.Since(requestStart))
-		return stream, nil
+	waitLog := time.NewTimer(clientTunnelSlowAfter)
+	defer waitLog.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			clog.Errorf(ctx, "unable to create tunnel to client %s for id %s after %s: %v", sessionID, id, time.Since(requestStart).Round(time.Millisecond), ctx.Err())
+			return nil, ctx.Err()
+		case stream := <-stCh:
+			if elapsed := time.Since(requestStart); elapsed > clientTunnelSlowAfter {
+				clog.Warnf(ctx, "created tunnel to client %s for id %s slowly in %s", sessionID, id, elapsed.Round(time.Millisecond))
+			} else {
+				clog.Debugf(ctx, "Created tunnel to client %s for id %s in %s", sessionID, id, elapsed)
+			}
+			return stream, nil
+		case <-waitLog.C:
+			clog.Warnf(
+				ctx,
+				"still waiting for client tunnel after %s: clientSession=%s conn=%s dialTimeout=%s roundtripLatency=%s",
+				time.Since(requestStart).Round(time.Millisecond),
+				sessionID,
+				id,
+				dialTimeout,
+				roundTripLatency,
+			)
+			waitLog.Reset(clientTunnelStillWaitingLog)
+		}
 	}
 }
 
