c805d04e56b924385b2c2455bd65225800c23c64
Breland Miley 🌈 <breland@openai.com>
2026-05-07 22:43:59 -0700

    The One With Telepresence Session Flap Observability

--- pkg/client/rootd/session.go
+++ pkg/client/rootd/session.go
@@ -1229,6 +1229,7 @@ func (s *session) hasPodConnectivity(info *manager.ClusterInfo) bool {
 }
 
 func (s *session) run(initErrs chan<- error) {
+	started := time.Now()
 	defer func() {
 		clog.Info(s, "-- session ended")
 	}()
@@ -1240,8 +1241,13 @@ func (s *session) run(initErrs chan<- error) {
 	}
 	close(initErrs)
 	err := g.Wait()
+	elapsed := time.Since(started).Round(time.Millisecond)
 	if err != nil {
-		clog.Errorf(s, "session ended with error: %v", err)
+		clog.Errorf(s, "session ended after %s with error: %v context=%v cause=%v", elapsed, err, s.Err(), context.Cause(s))
+	} else if s.Err() != nil {
+		clog.Infof(s, "session context ended after %s: context=%v cause=%v", elapsed, s.Err(), context.Cause(s))
+	} else {
+		clog.Infof(s, "session services stopped cleanly after %s", elapsed)
 	}
 }
 
