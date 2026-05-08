e4041aa58af753ed3575fa55a7f12aaae9f16dca
Breland Miley 🌈 <breland@openai.com>
2026-05-07 23:42:23 -0700

    The One With Reused HTTP Intercept Transports

--- cmd/traffic/cmd/agent/fwd/tcp.go
+++ cmd/traffic/cmd/agent/fwd/tcp.go
@@ -23,9 +23,10 @@ var errClientStream = errors.New("failed to create client stream")
 
 type tcp struct {
 	*interceptor
-	tlsManager     tls.Manager
-	listenerSwitch ListenerSwitch
-	httpRequestID  atomic.Uint64
+	tlsManager         tls.Manager
+	listenerSwitch     ListenerSwitch
+	httpRequestID      atomic.Uint64
+	httpTransportCache sync.Map
 }
 
 func NewTCPInterceptor(ctx context.Context, listenPort types.PortAndProto, tag tunnel.Tag, tlsManager tls.Manager, target netip.AddrPort) Interceptor {
@@ -42,6 +43,7 @@ func (f *tcp) IsHTTP() bool {
 // SetIntercepting overrides the base implementation to handle HTTP intercepts.
 func (f *tcp) SetIntercepting(intercepts []*manager.InterceptInfo) {
 	f.interceptor.SetIntercepting(intercepts)
+	f.pruneHTTPInterceptTransports(intercepts)
 	f.setListenerSwitch()
 }
 
